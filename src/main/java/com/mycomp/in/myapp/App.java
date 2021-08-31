7911221ad121ac730c5ca3fe47b681c7b5845ef4
nayan <nayan.verma@debutinfotech.com>
2021-08-31 10:26:52 +0530

    Initial commit

---
+++ src/main/java/com/mycomp/in/myapp/App.java
@@ -0,0 +1,13 @@
+package com.mycomp.in.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
