eca7e18ca63c6af11905a8f20d06eb378015fa49
nayan <nayanrahul.jnv@gmail.com>
2021-09-23 11:24:03 +0530

    Reduce by adding lambda expression

--- src/main/java/com/mycomp/in/implementaion/Anonymous.java
+++ src/main/java/com/mycomp/in/implementaion/Anonymous.java
@@ -3,11 +3,7 @@ package com.mycomp.in.implementaion;
 import com.mycomp.in.intities.IAnonymous;
 
 public class Anonymous {
-    IAnonymous data = new IAnonymous(){
-        
-        @Override 
-        public String name(){
-            return "Nayan";
-        }
+    IAnonymous data = () -> {
+        return "Nayan";
     };
 }
